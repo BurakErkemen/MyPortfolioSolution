3207ec4a84fcc04e5b7329daefa0e2a99108c0d9
Burak Furkan ERKEMEN <84676805+BurakErkemen@users.noreply.github.com>
2025-03-07 23:41:23 +0300

    UserService
    User Service metotları güncelleniyor ve ekleniyor.

--- MyPortfolioSolution/Portfolio.Service/Services/Users/IUserService.cs
+++ MyPortfolioSolution/Portfolio.Service/Services/Users/IUserService.cs
@@ -1,4 +1,5 @@
-﻿using Portfolio.Services.Services.Users.Update;
+﻿using Portfolio.Services.Services.Users.Create;
+using Portfolio.Services.Services.Users.Update;
 
 namespace Portfolio.Services.Services.Users
 {
@@ -7,5 +8,11 @@ namespace Portfolio.Services.Services.Users
         Task<ServiceResult> DeactivateAccountAsync(UpdateUserRequest request);// Hesabı pasif hale getir.
         Task<ServiceResult> ReactivateAccountAsync(UpdateUserRequest request);// Hesabı tekrar aktif hale getir.
         Task<ServiceResult> UpdateUserRoleAsync(UpdateUserRequest request); // Admin, yazar gibi rolleri güncelle.
+
+        Task<ServiceResult<UserResponse?>> GetAllAsync();
+        Task<ServiceResult<UserResponse?>> GetByIdAsync(int id);
+        Task<ServiceResult<CreateUserResponse>> CreateAsync(CreateUserRequest request);
+        Task<ServiceResult> UpdateAsync(UpdateUserRequest request);
+        Task<ServiceResult> DeleteAsync(int id);
     }
 }
--- MyPortfolioSolution/Portfolio.Service/Services/Users/UserService.cs
+++ MyPortfolioSolution/Portfolio.Service/Services/Users/UserService.cs
@@ -1,5 +1,6 @@
 ﻿using Portfolio.Repositories.Models.Users;
 using Portfolio.Repositories.UnitOfWorkRepositories;
+using Portfolio.Services.Services.Users.Create;
 using Portfolio.Services.Services.Users.Update;
 using System.Net;
 
@@ -7,13 +8,11 @@ namespace Portfolio.Services.Services.Users
 {
     public class UserService(IUserRepository userRepository, IUnitOfWorkRepository unitOfWork) : IUserService
     {
-        public async Task<ServiceResult> DeactivateAccountAsync(UpdateUserRequest request)
+        public async Task<ServiceResult> DeactivateAccountAsync(UpdateUserRequest request) // Hesabı pasif hale getir.
         {
-            var user = await userRepository.GetByIdAsync(request.Id);
-            if (user == null)
-            {
-                return ServiceResult.Fail("User not found", HttpStatusCode.NotFound);
-            }
+            var user = await userRepository.GetByEmailAsync(request.Email);
+            if (user == null) return ServiceResult.Fail("User not found", HttpStatusCode.NotFound);
+            
             // Kullanıcının hesabını pasif hale getir
             user.IsActivate = request.AccountStatus;
             userRepository.UpdateAsync(user);
@@ -21,16 +20,45 @@ namespace Portfolio.Services.Services.Users
 
             return ServiceResult.Success(HttpStatusCode.NoContent);
         }
+        public async Task<ServiceResult> ReactivateAccountAsync(UpdateUserRequest request) // Hesabı tekrar aktif hale getir.
+        {
+            var user = await userRepository.GetByEmailAsync(request.Email);
+            if (user == null) return ServiceResult.Fail("User not found!", HttpStatusCode.NotFound);
 
-        public Task<ServiceResult> ReactivateAccountAsync(UpdateUserRequest request)
+            // Kullanıcının hesabını aktif hale getir 
+            user.IsActivate = request.AccountStatus;
+            userRepository.UpdateAsync(user);
+            await unitOfWork.SaveChangesAsync();
+
+            return ServiceResult.Success(HttpStatusCode.NoContent);
+        }
+        public Task<ServiceResult> UpdateUserRoleAsync(UpdateUserRequest request) // Admin, yazar gibi rolleri güncelle.
+        {
+            var user = userRepository.GetByIdAsync(request.Id);
+            if (user == null) return ServiceResult.Fail("User not found!", HttpStatusCode.NotFound);
+
+            // Rol güncelleme
+            
+        }
+        public Task<ServiceResult<UserResponse?>> GetAllAsync()
         {
             throw new NotImplementedException();
         }
-
-        public Task<ServiceResult> UpdateUserRoleAsync(UpdateUserRequest request)
+        public Task<ServiceResult<UserResponse?>> GetByIdAsync(int id)
         {
             throw new NotImplementedException();
         }
-
+        public Task<ServiceResult<CreateUserResponse>> CreateAsync(CreateUserRequest request)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ServiceResult> UpdateAsync(UpdateUserRequest request)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ServiceResult> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
