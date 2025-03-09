78b1d3a222699274c72888a64dcfd7b353f5aebc
Burak Furkan ERKEMEN <84676805+BurakErkemen@users.noreply.github.com>
2025-03-09 06:43:02 +0300

    Update ContactFormController
    Controller bitirildi.

--- MyPortfolioSolution/Portfolio.API/Controllers/ContactFormController.cs
+++ MyPortfolioSolution/Portfolio.API/Controllers/ContactFormController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Portfolio.Services.Services.ContactForms;
+using Portfolio.Services.Services.ContactForms.Create;
 
 namespace Portfolio.API.Controllers
 {
@@ -8,6 +9,45 @@ namespace Portfolio.API.Controllers
     [ApiController]
     public class ContactFormController(IContactFormService contactFormService) : CustomBaseController
     {
+        [HttpGet("getAll")]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            return CreateActionResult(await contactFormService.GetAllAsync());
+        }
 
+
+        [HttpGet("getByName/{name:string}")]
+        public async Task<IActionResult> GetByNameAsync(string name)
+        {
+            return CreateActionResult(await contactFormService.GetByNameAsync(name));
+        }
+
+
+        [HttpGet("getByEmail/{email:string}")]
+        public async Task<IActionResult> GetByEmailAsync(string email)
+        {
+            return CreateActionResult(await contactFormService.GetByEmailAsync(email));
+        }
+
+
+        [HttpGet("getByCompanyName/{companyName:string}")]
+        public async Task<IActionResult> GetByCompanyName(string companyName)
+        {
+            return CreateActionResult(await contactFormService.GetByCompanyNameAsync(companyName));
+        }
+
+
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateAsync(CreateContactFormRequest request)
+        {
+            return CreateActionResult(await contactFormService.CreateAsync(request));
+        }
+
+
+        [HttpDelete("delete/{id:int}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            return CreateActionResult(await contactFormService.DeleteAsync(id));
+        }
     }
 }
